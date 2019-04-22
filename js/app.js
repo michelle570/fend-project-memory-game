1cf6e2ee72086afbd0b27aff7a2440efa506af8f
michelle570 <49167773+michelle570@users.noreply.github.com>
2019-04-22 12:34:49 -0400

    April 22
    Pending Transitions and disable deck on cards
    Pending Timer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-/*NOTE: Create a list that holds all of your cards */
-/*NOTE: Maybe just create array of list of classes*/
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
+/*List of Classes for Cards*/
 let cardArray = ['fa-key', 'fa-key', 'fa-leaf', 'fa-leaf', 'fa-paw', 'fa-paw', 'fa-rocket', 'fa-rocket', 'fa-umbrella', 'fa-umbrella', 'fa-paper-plane', 'fa-paper-plane', 'fa-bolt', 'fa-bolt', 'fa-book', 'fa-book'];
 
 /*Open Card*/
@@ -17,42 +8,62 @@ let Moves = 0;
 
 const deck = document.querySelector('#deck');
 const resetBtn = document.querySelector('#restart');
+const playAgainBtn = document.querySelector('#PlayAgain');
+
+//Reset on Load
+document.addEventListener('DOMContentLoaded', function () {
+    ShuffledContainer();
+});
 
 //RESET Click Function
 resetBtn.addEventListener('click', function(event) {
   ShuffledContainer();
 });
 
+//Modal Button Click Function
+playAgainBtn.addEventListener('click', function(event) {
+  /*CLOSE MODAL*/
+  modal.style.display = "none";
+
+  /*Shuffle/Restart Cards*/
+  ShuffledContainer();
+});
+
 /*Click Event*/
 deck.addEventListener('click', function(event) {
-  /*Sets Current Card Selected*/
+  /*Sets Current Card Clicked*/
   let card = event.target;
 
-  /*If not matched card then Open Card*/
-  if (!(card.classList.contains('match')) && !(card.classList.contains('open')) && !(deck.classList.contains('disable'))) {
+  /*Verifies if card is not already opened, already been matched, is temporarily disabled or is the deck area*/
+  if (!(card.classList.contains('match')) && !(card.classList.contains('open')) && !(deck.classList.contains('disable')) && (card.classList.contains('card')) ){
+
     card.classList.toggle('open');
+
+
     /*Add to Moves*/
     Moves ++;
     document.querySelector('.moves').textContent = Moves;
     setStars();
 
-    /*Verifies agains Opened Card*/
+    /*Verifies if there is an open card*/
     if (!OpenCard) {
       /*Set Open Card on First Click*/
       OpenCard = card;
     }
     else {
       /*Compare Current Card with Just Opened on Second Click*/
+
       if (card.innerHTML == OpenCard.innerHTML) {
-        /*same*/
-        console.log("same");
+        /*Cards are a match*/
         card.classList.toggle('match');
         OpenCard.classList.toggle('match');
 
-        /*Add into Score*/
+        /*Add into Score & Resets Open Card*/
         Score ++;
         document.querySelector('.score').textContent = Score + "/8";
         OpenCard = "";
+
+        /*Verifies if match makes the user win*/
         if (Score == 8) {
           console.log("WON GAME!");
           GameWon();
@@ -60,8 +71,7 @@ deck.addEventListener('click', function(event) {
 
       }
       else {
-        /*Not Same*/
-        console.log("not same");
+        /*Cards are not a match*/
 
         /*Flip Back Cards*/
         card.classList.add('unmatch');
@@ -70,9 +80,8 @@ deck.addEventListener('click', function(event) {
         /*Disable board temporarily*/
         deck.classList.add('disable');
 
+
         setTimeout (function() {
-          console.log(card);
-          console.log(OpenCard);
           card.classList.remove('open', 'unmatch');
           OpenCard.classList.remove('open', 'unmatch');
           /*clear OpenCard*/
@@ -80,7 +89,7 @@ deck.addEventListener('click', function(event) {
 
           /*Enable deck again*/
           deck.classList.remove('disable');
-        }, 1100);
+        }, 1800);
 
       }
     }
@@ -89,31 +98,32 @@ deck.addEventListener('click', function(event) {
 });
 
 
-function GameWon() {
-  modal.style.display = "block";
-  /*Display Modal Info*/
-  //FIX THIS INFORMATION DISPLAYED
-  document.querySelector('.win-score').textContent = Score;
-  document.querySelector('.win-stars').textContent = Moves;
+//FUNCTIONS
 
-  /*NOTE: ADD reset function*/
+function GameWon() {
+  /*Display Modal Info*/
+  modal.style.display = "block";
+  document.querySelector('.win-moves').textContent = Moves;
+  document.querySelector('.win-stars').innerHTML = document.querySelector('.stars').innerHTML;
 }
 
 function setStars() {
-  /*Set Stars*/
-  displayStars = document.querySelector('.win-stars');
-   if (Moves > 16) {
-     /*set 2 stars*/
+  let stars = "";
+
+   if (Moves == 16) {
+     stars = "<li><i class='fas fa-star'></i></li><li><i class='fas fa-star'></i></li><li><i class='far fa-star'></i></i>"
+     document.querySelector('.stars').innerHTML = stars;
    }
-   else if(Moves > 32) {
-     /*Set 1 Star*/
+   else if(Moves == 32) {
+     stars = "<li><i class='fas fa-star'></i></li><li><i class='far fa-star'></i></li><li><i class='far fa-star'></i></i>"
+     document.querySelector('.stars').innerHTML = stars;
    }
-   else if (Moves > 48) {
-     /*Set 0 star*/
+   else if (Moves == 48) {
+     stars = "<li><i class='far fa-star'></i></li><li><i class='far fa-star'></i></li><li><i class='far fa-star'></i></i>"
+     document.querySelector('.stars').innerHTML = stars;
    }
 
 }
-/*SET EVERYTHING IN FUNCTIONS*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -132,32 +142,35 @@ function shuffle(array) {
 
 function ShuffledContainer() {
   /*Shuffle Array*/
-  cardArray = shuffle(cardArray);
-  console.log(cardArray);
+  shuffle(cardArray);
+
   //create new container...
+  let newContainer = "";
+  for (const cardclass of cardArray) {
+    newContainer += "<li class='card'><i class='fas " + cardclass +"'></i></li>";
+  }
+  deck.innerHTML = newContainer;
 
-  //Reset All Values
-
-  //Close All Cards
+  /*Reset Scores and Stars*/
+  Reset();
 }
 
+function Reset() {
+  Score = 0;
+  Moves = 0;
+  OpenCard = "";
+  document.querySelector('.stars').innerHTML = "<li><i class='fas fa-star'></i></li><li><i class='fas fa-star'></i></li><li><i class='fas fa-star'></i></i>";
+  document.querySelector('.moves').textContent = Moves;
+  document.querySelector('.score').textContent = Score + "/8";
+}
 
 /*MODAL FUNCTIONS adapted from https://www.w3schools.com/howto/howto_css_modals.asp*/
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the <span> element that closes the modal
 var CloseSpan = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  document.querySelector('.win-score').textContent = Score;
-  document.querySelector('.win-stars').textContent = Moves;
-}
 
 // When the user clicks on <span> (x), close the modal
 CloseSpan.onclick = function() {
